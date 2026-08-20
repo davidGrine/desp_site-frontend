1b2b7cd11ffaaf5581d381a61da521204de1766d
davidGrine <grindavid8@gmail.com>
2026-08-20 15:28:38 +0300

    Modified global styles and layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,29 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Roboto } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const roboto = Roboto({ 
+  variable: "--font-roboto",
+  subsets: ["latin", "cyrillic"] 
 });
 
 export const metadata: Metadata = {
-  title: "Devlog.exe",
+  title: {
+    template: "%s | Devlog.exe",
+    default: "Devlog.exe",
+  },
   description: "Practice my skills",
+  icons: {
+    icon: "/avatar.png",
+  },
 };
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      data-theme="light"
+      className={`${roboto.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
