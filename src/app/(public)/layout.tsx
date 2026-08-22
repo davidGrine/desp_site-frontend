6a272837f54f9844f82dfae2036e079914d5728d
davidGrine <grindavid8@gmail.com>
2026-08-22 15:35:45 +0300

    Nothing.

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,5 +1,4 @@
 import { Header } from '@/src/components/Header'
-import { NavSideBar } from '@/src/components/NavSideBar'
 import type { PropsWithChildren } from 'react'
 
 export default function Layout({ children }: PropsWithChildren<unknown>) {
